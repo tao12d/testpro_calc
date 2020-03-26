e3e0b2c3ca4a351077fe1fb311f3cb32abb267a1
tao12d <233655887@qq.com>
2020-03-26 12:01:36 +0800

    第一次作业提交

--- testing/test_pytest.py
+++ testing/test_pytest.py
@@ -3,42 +3,35 @@ import pytest
 
 
 class TestCalc:
+    def setup(self):
+        self.calc = Calc()
+
     def test_add1(self):
-        calc = Calc()
-        assert calc.add(1, 2) == 3
+        assert self.calc.add(1, 2) == 3
 
     def test_add2(self):
-        calc = Calc()
-        assert calc.add(1/2, 1/2) == 1
+        assert self.calc.add(1 / 2, 1 / 2) == 1
 
     def test_add3(self):
-        calc = Calc()
-        assert calc.add(1, 2) != 1
+        assert self.calc.add(1, 2) != 1
 
     def test_add4(self):
-        calc = Calc()
-        assert calc.add(0.01, 0.02) == 0.03
+        assert self.calc.add(0.01, 0.02) == 0.03
 
     def test_div1(self):
-        calc = Calc()
-        assert calc.div(4, 2) == 2
+        assert self.calc.div(4, 2) == 2
 
     def test_div2(self):
-        calc = Calc()
-        assert calc.div(2, 4) == 0.5
+        assert self.calc.div(2, 4) == 0.5
 
     def test_div3(self):
-        calc = Calc()
-        assert calc.div(0, 2) == 0
+        assert self.calc.div(0, 2) == 0
 
     def test_div4(self):
-        calc = Calc()
-        assert calc.div(4, 3) != 3
+        assert self.calc.div(4, 3) != 3
 
     def test_div5(self):
-        calc = Calc()
-        assert calc.div(4, 3) != 3
+        assert self.calc.div(4, 3) != 3
 
     def test_div5(self):
-        calc = Calc()
-        assert calc.div(2, 0) == "ZeroDivisionError: division by zero"
+        assert self.calc.div(2, 0) == "ZeroDivisionError: division by zero"
