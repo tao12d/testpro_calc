512b1bf2678e05b3cb65c9a82cb054dc4766decd
tao12d <233655887@qq.com>
2020-03-26 11:26:06 +0800

    第一作业提交

--- testing/test_pytest.py
+++ testing/test_pytest.py
@@ -7,14 +7,38 @@ class TestCalc:
         calc = Calc()
         assert calc.add(1, 2) == 3
 
+    def test_add2(self):
+        calc = Calc()
+        assert calc.add(1/2, 1/2) == 1
+
     def test_add3(self):
         calc = Calc()
-        assert calc.add(1 / 2, 1 / 2) == 1
+        assert calc.add(1, 2) != 1
 
     def test_add4(self):
         calc = Calc()
-        assert calc.add(1, 2) == 1
+        assert calc.add(0.01, 0.02) == 0.03
 
-    def test_add2(self):
+    def test_div1(self):
         calc = Calc()
-        assert calc.add(0.1, 0.2) == 0.3
+        assert calc.div(4, 2) == 2
+
+    def test_div2(self):
+        calc = Calc()
+        assert calc.div(2, 4) == 0.5
+
+    def test_div3(self):
+        calc = Calc()
+        assert calc.div(0, 2) == 0
+
+    def test_div4(self):
+        calc = Calc()
+        assert calc.div(4, 3) != 3
+
+    def test_div5(self):
+        calc = Calc()
+        assert calc.div(4, 3) != 3
+
+    def test_div5(self):
+        calc = Calc()
+        assert calc.div(2, 0) == "ZeroDivisionError: division by zero"
